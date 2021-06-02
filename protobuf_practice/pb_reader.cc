f999857009db6a91c49c4e75e32dd751b6410eb0
tianchenge <tianchenge@tencent.com>
2021-06-03 01:28:50 +0800

    proto practice 2

--- protobuf_practice/pb_reader.cc
+++ protobuf_practice/pb_reader.cc
@@ -8,7 +8,16 @@ using namespace std;
 
 // print the information of the given message
 void readMessage(TopMessage* message) {
-    cout << "Message ID:" << message -> id() << endl;
+    cout << "Message ID :" << message -> id() << endl;
+    cout << "Message val: " << message -> val() << endl;
+    cout << "Message name: " << message -> name() << endl;
+    cout << "Message checked: " << message -> checked() << endl;
+}
+
+// This function checks if a file exists
+bool fileExists(const char *fileName) {
+    ifstream infile(fileName);
+    return infile.good();
 }
 
 // Main function: generate deserialze the messages and print them
@@ -16,24 +25,50 @@ void readMessage(TopMessage* message) {
 int main(int argc, char* argv[]) {
     // GOOGLE_PROTOBUF_VERIFY_VERSION;
 
+    if (argc > 3) {
+        cerr << "Too many arguments." << endl;
+        return -1;
+    }
+
+    // argv[1] is number of repeats, default 5
+    // argv[2] is the path of folder for messages, default ../files/messageX.txt
     int repeat = 5;
+    string path = "files";
+    
+    if (argc > 1 and atoi(argv[1]) > 0) {
+        repeat = atoi(argv[1]);
+    }
+    cout << "The Value of repeat is " << repeat << endl;
+
+    if (argc > 2 and *argv[2]) {
+        path = argv[2];
+    }
+    if (!fileExists(&path[0])) {
+        cerr << "File does not exist." << endl;
+        return -1;
+    }
+    cout << "The path for messages is " << path << endl;
 
     // repeat generating one message
     for(int i = 0; i < repeat; i ++) {
-        // create the local file
-        string fileName = "files/message" + to_string(i) + ".txt";
+        // check the local file, return error if not exist
+        string fileName = path + "/message" + to_string(i) + ".txt";
         cout << "Current file: " << fileName;
         cout << endl;
         char* charFileName = &fileName[0];
+        if (!fileExists(charFileName)) {
+            cerr << "File does not exist." << endl;
+            return -1;
+        }
         TopMessage message;
 
+        // deserialize and read the message
         ifstream messageFile;
         messageFile.open(fileName, ios::in | ios::binary);
-        int messageSize;
         string serializedMessage;
         messageFile >> serializedMessage;
         message.ParseFromString(serializedMessage);
-        cout << "Message ID:" << message.id() << endl;
+        readMessage(&message);
         messageFile.close();
     }
 
--- protobuf_practice/pb_writer.cc
+++ protobuf_practice/pb_writer.cc
@@ -6,10 +6,36 @@
 #include "../dst/topmessage.pb.h"
 using namespace std;
 
-// This function fills in a TopMessage with random numbers
+// This function generates a random string
+string generateRandomString() {
+    size_t length = sizeof(string);
+    const char* charmap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const size_t charmapLength = strlen(charmap);
+    auto generator = [&](){ return charmap[rand()%charmapLength]; };
+    string result;
+    result.reserve(length);
+    generate_n(back_inserter(result), length, generator);
+    return result;
+}
+
+// This function fills a TopMessage with random values for each field
 void generateOne(TopMessage* message) {
     message -> set_id(rand());
-    cout<< "Message ID:"<< message -> id() << endl;
+    message -> set_val(((double)rand()/(double)RAND_MAX));
+    message -> set_name(generateRandomString());
+    message -> set_checked(rand() % 2 == 0);
+
+    // print out all values for checking
+    cout << "Message ID: " << message -> id() << endl;
+    cout << "Message val: " << message -> val() << endl;
+    cout << "Message name: " << message -> name() << endl;
+    cout << "Message checked: " << message -> checked() << endl;
+}
+
+// This function checks if a file exists
+bool fileExists(const char *fileName) {
+    ifstream infile(fileName);
+    return infile.good();
 }
 
 // Main function: generate a # (specified by user) of messages and serialize them
@@ -17,23 +43,34 @@ void generateOne(TopMessage* message) {
 int main(int argc, char* argv[]) {
     // GOOGLE_PROTOBUF_VERIFY_VERSION;
 
-    if (argc > 1) {
+    if (argc > 3) {
         cerr << "Too many arguments." << endl;
         return -1;
     }
 
-    // argv[0] is number of repeats, default 500
-    int repeat = atoi(argv[0]);
-    if (repeat == 0) {
-        repeat = 5;
+    // argv[1] is number of repeats, default 5
+    // argv[2] is the path of folder for messages, default ../files/messageX.txt
+    int repeat = 5;
+    string path = "files";
+    
+    if (argc > 1 and atoi(argv[1]) > 0) {
+        repeat = atoi(argv[1]);
     }
-    cout << "The Value of repeat is " << repeat;
-    cout << endl;
+    cout << "The Value of repeat is " << repeat << endl;
+
+    if (argc > 2 and *argv[2]) {
+        path = argv[2];
+    }
+    if (!fileExists(&path[0])) {
+        cerr << "File does not exist." << endl;
+        return -1;
+    }
+    cout << "The path for messages is " << path << endl;
 
     // repeat generating one message
     for(int i = 0; i < repeat; i ++) {
         // create the local file
-        string fileName = "files/message" + to_string(i) + ".txt";
+        string fileName = path + "/message" + to_string(i) + ".txt";
         cout << "Current file: " << fileName;
         cout << endl;
         char* charFileName = &fileName[0];
