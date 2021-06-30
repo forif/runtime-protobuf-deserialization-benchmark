2bb0ec1dff9c5c1e59c0590adcda2c7797ab6643
tianchenge <tianchenge@tencent.com>
2021-06-30 17:01:58 +0800

    python script added

--- script.py
+++ script.py
@@ -1,2 +1,4 @@
-import os
-os.syste
+import subprocess
+# subprocess.run(['/protobuf_practice/static_reader', '10000', 'messages.txt'])
+subprocess.run(["./static_reader", "10000", "messages.txt"], cwd = "protobuf_practice")
+subprocess.run(["./dynamic_reader", "../../lib/protolib/crestmessage.proto", "../protobuf_practice/messages.txt", "CrestMessage", "10000"], cwd = "protobuf_noProto")
