cd7577a5e9fd6182b6182b098479815007ea6401
nemoforwang <nemoforwang@tencent.com>
2021-08-10 14:41:04 +0800

    update nodejs reader performance computation.

--- pb_dyn/nodejs_reader.js
+++ pb_dyn/nodejs_reader.js
@@ -1,7 +1,6 @@
 const protobuf = require("protobufjs");
 const fs = require("fs");
-const util = require('util');
-const { toNamespacedPath } = require("path");
+const { PerformanceObserver, performance } = require('perf_hooks');
 
 
 // argv[2] is the path of given .proto file that contains the protobuf definition
@@ -32,61 +31,56 @@ protobuf.load(proto_path, function (err, root) {
 		var totalTime = 0;
 		var totalBytes = 0;
 
-		// create a promise version of fs.read()
-		fs.readAsync = util.promisify(fs.read);
-
 		// a function that reads the size of message (4 bytes)
-		async function read_size () {
+		function read_size () {
 			var sizeBuffer = new Buffer.alloc(4);
 			var size = 0;
-			await fs.readAsync(fd, sizeBuffer, 0, 4, pos).then(data => {
-        size = data['buffer'].readInt32LE(0);
-				pos += 4;
-			});
-
+			fs.readSync(fd, sizeBuffer, 0, 4, pos);
+      size = sizeBuffer.readInt32LE(0);
+			pos += 4;
 			return size;
 		}
 
 		// a function that reads the message
-		async function read_message (size) {
+		function read_message (size) {
 			var buffer = new Buffer.alloc(size);
-			await fs.readAsync(fd, buffer, 0, size, pos).then(data => {
-				pos += size;
-				i += 1;
+			fs.readSync(fd, buffer, 0, size, pos);
+			pos += size;
+			i += 1;
 
-				// keep track of time and bytes taken by decoding the array to message and converting it to json object
-				var hrstart = process.hrtime();
-				let msg = message.decode(buffer);
+			// keep track of time and bytes taken by decoding the array to message and converting it to json object
+      let mstart = performance.now();
+			let msg = message.decode(buffer);
 
-				// print out the deserialized message
-				let object = message.toObject(msg, {
-					longs: String,
-					enums: String,
-					bytes: String,
-				});
-				
-				var hrend = process.hrtime(hrstart);
-				totalTime += (hrend[0] + hrend[1] / 1000000000);
-				totalBytes += size;
+			// print out the deserialized message
+			let object = message.toObject(msg, {
+				longs: String,
+				enums: String,
+				bytes: String,
 			});
+			
+      let mend = performance.now();
+      totalTime += (mend - mstart) / 1000;
+			totalBytes += size;
 		}
 
 		// a function that reads the size and then the content of the message
-		async function read_whole () {
-			message_size = await read_size();
-			await read_message(message_size);
+		function read_whole () {
+			message_size = read_size();
+			read_message(message_size);
 		}
 
 		// repeat reading for the given number of times
 		const message = root.lookupType(message_name);
-    repeats = await read_size();
+    repeats = read_size();
     console.log("total " + repeats + " messages in file " + file_path);
+    let start = performance.now();
 		for (let i = 0; i < repeats; i ++) {
-			await read_whole();
+			read_whole();
 		}
 
 		// print results
-		console.log("Performance of Nodejs dynamic reader: " + totalTime + "s");
+		console.log("Performance of Nodejs dynamic reader: " + totalTime + "s " + ((performance.now() - start) / 1000) + "s");
 		console.log("Number of messages processed per second: " + repeats / totalTime);
 		console.log("Number of bytes processed per second: " + totalBytes / totalTime);
 	});
